281eb8af299b05b02a2c054c4f6e6db847853507
Eric Sheppard <eric@sheppard.cloud>
2021-09-26 08:39:11 +0000

    add pk comparisons to rust codegen

--- codegen/src/rust.rs
+++ codegen/src/rust.rs
@@ -1,4 +1,4 @@
-use heck::{CamelCase, ShoutySnakeCase, TitleCase};
+use heck::{CamelCase, ShoutySnakeCase, SnakeCase, TitleCase};
 use std::{collections, fs, str};
 
 use crate::{mms, pdr};
@@ -133,15 +133,30 @@ impl pdr::Report {
             format!("{}{}", self.name.to_camel_case(), self.version)
         }
     }
+    pub fn get_rust_pk_name(&self) -> String {
+        format!("{}PrimaryKey", self.get_rust_struct_name())
+    }
+    pub fn get_partition_base(&self) -> String {
+        if let Some(sub_type) = &self.sub_type {
+            format!("{}_{}_v{}", 
+                self.name.to_snake_case(),
+                sub_type.to_snake_case(),
+                self.version,
+            )
+        } else {
+            format!("{}_v{}", 
+                self.name.to_snake_case(),
+                self.version,
+            )
+        }
+    }
     pub fn get_rust_file_key_literal(&self) -> String {
         format!(
-            r#"
-            crate::FileKey {{
-                data_set_name: "{data_set}".into(),
-                table_name: Some("{table}".into()),
-                version: {version},
-            }}
-            "#,
+            r#"crate::FileKey {{
+    data_set_name: "{data_set}".into(),
+    table_name: Some("{table}".into()),
+    version: {version},
+}}"#,
             data_set = self.name.to_shouty_snake_case(),
             table = if let Some(sub_type) = &self.sub_type {
                 sub_type
@@ -206,7 +221,7 @@ pub fn run() -> anyhow::Result<()> {
                     .derive("Eq")
                     .derive("serde::Deserialize")
                     .derive("serde::Serialize");
-                for col in table.columns.columns {
+                for col in table.columns.columns.iter() {
                     if &col.field_name() == "type" {
                         let mut field = codegen::Field::new("pub type_", &col.to_rust_type());
                         field.annotation(vec!["#[serde(rename = \"type\")]"]);
@@ -258,13 +273,166 @@ pub fn run() -> anyhow::Result<()> {
                 get_file_key.line(&pdr_report.get_rust_file_key_literal());
 
                 current_impl.push_fn(get_file_key);
+
+
+                let mut primary_key = codegen::Function::new("primary_key");
+                primary_key.ret(&pdr_report.get_rust_pk_name());
+                primary_key.arg_ref_self();
+                primary_key.line(&format!(
+                    r#"{pk_name} {{
+{pk_fields}
+}}"#,
+                    pk_name = pdr_report.get_rust_pk_name(),
+                    pk_fields = table.primary_key_columns.cols.iter()
+                        .map(|name| format!("    {0}: self.{0}.clone()", name.to_lowercase()))
+                        .collect::<Vec<String>>()
+                        .join(",\n"),
+                ));
+
+                current_impl.push_fn(primary_key);
+
+
+                let mut compare_with_key = codegen::Function::new("compare_with_key");
+                compare_with_key.ret("bool");
+                compare_with_key.line(&table.primary_key_columns.cols.iter()
+                    .map(|name| format!("self.{0} == other.{0}", name.to_lowercase()))
+                    .collect::<Vec<String>>()
+                    .join("\n&& "),
+                );                
+                compare_with_key.arg_ref_self();
+                compare_with_key.arg("other", &format!("&{}", pdr_report.get_rust_pk_name()));
+
+                current_impl.push_fn(compare_with_key);
+
+
+                let mut compare_with_other = codegen::Function::new("compare_with_other");
+                compare_with_other.ret("bool");
+                compare_with_other.arg_ref_self();
+                compare_with_other.arg("other", "&Self");
+                compare_with_other.line(&table.primary_key_columns.cols.iter()
+                    .map(|name| format!("self.{0} == other.{0}", name.to_lowercase()))
+                    .collect::<Vec<String>>()
+                    .join("\n&& "),
+                );    
+                current_impl.push_fn(compare_with_other);
+
+
+                let mut partition_suffix = codegen::Function::new("partition_suffix");
+                partition_suffix.ret("String");
+                partition_suffix.arg_ref_self();
+
+                if table.primary_key_columns.cols.iter().any(|c| c.to_lowercase() == "settlementdate") {
+
+                    partition_suffix.line(r#"format!("{}_{}", chrono::Datelike::year(self.settlementdate), chrono::Datelike::month(self.settlementdate))"#);
+                } else {
+                    partition_suffix.line("String::new()");
+                }
+                current_impl.push_fn(partition_suffix);
+
+                let mut partition_name = codegen::Function::new("partition_name");
+                partition_name.ret("String");
+                partition_name.arg_ref_self();
+
+                if table.primary_key_columns.cols.iter().any(|c| c.to_lowercase() == "settlementdate") {
+
+                    partition_name.line(
+                        &format!(
+                            r#"format!("{}_{{}}_{{}}", chrono::Datelike::year(self.settlementdate), chrono::Datelike::month(self.settlementdate))"#,
+                            pdr_report.get_partition_base()
+                        )
+                    );
+                } else {
+                    partition_name.line(&format!(r#""{}".to_string()"#, pdr_report.get_partition_base()));
+                }
+
+
+                current_impl.push_fn(partition_name);
+
                 current_impl.fmt(&mut fmtr)?;
+
+
+                let mut pk_struct = codegen::Struct::new(&pdr_report.get_rust_pk_name());
+                pk_struct
+                    .vis("pub")
+                    .derive("Debug")
+                    .derive("Clone")
+                    .derive("PartialEq")
+                    .derive("Eq")
+                    .derive("PartialOrd")
+                    .derive("Ord");
+
+                for pk_col_name in table.primary_key_columns.cols.iter() {
+                    let col = table.columns.columns.iter().find(|col| &col.name == pk_col_name).expect("PK column must exist");
+                    
+                    // temporary
+                    if !col.mandatory {
+                        panic!("Non mandatory column in primary key: {:?}", col);
+                    }
+
+                    if &col.field_name() == "type" {
+                        let field = codegen::Field::new("pub type_", &col.to_rust_type());
+                        pk_struct.push_field(field);
+                    } else if col.comment.contains("YYYYMMDDPPP") {
+                        // parse as DispatchPeriod
+                        let field = codegen::Field::new(
+                            &format!("pub {}", col.field_name()),
+                            "crate::DispatchPeriod",
+                        );
+                        pk_struct.push_field(field);
+                    } else if col.comment.contains("YYYYMMDDPP") {
+                        // parse as TradingPeriod
+                        let field = codegen::Field::new(
+                            &format!("pub {}", col.field_name()),
+                            "crate::TradingPeriod",
+                        );
+                        pk_struct.push_field(field);
+                    } else if col.data_type == mms::DataType::Date {
+                        let field = codegen::Field::new(
+                            &format!("pub {}", col.field_name()),
+                            &col.to_rust_type(),
+                        );
+                        pk_struct.push_field(field);
+                    } else {
+                        let field = codegen::Field::new(
+                            &format!("pub {}", col.field_name()),
+                            &col.to_rust_type(),
+                        );
+                        pk_struct.push_field(field);
+                    };
+                }
+
+                pk_struct.fmt(&mut fmtr)?;
+
+                let mut pk_impl = codegen::Impl::new(&pdr_report.get_rust_pk_name());
+
+                let mut compare_with_other_pk = codegen::Function::new("compare_with_other");
+                compare_with_other_pk.ret("bool");
+                compare_with_other_pk.arg_ref_self();
+                compare_with_other_pk.arg("other", "&Self");
+                compare_with_other_pk.line(&table.primary_key_columns.cols.iter()
+                    .map(|name| format!("self.{0} == other.{0}", name.to_lowercase()))
+                    .collect::<Vec<String>>()
+                    .join("\n&& "),
+                );    
+                pk_impl.push_fn(compare_with_other_pk);
+
+                let mut compare_with_row = codegen::Function::new("compare_with_row");
+                compare_with_row.ret("bool");
+                compare_with_row.arg_ref_self();
+                compare_with_row.arg("row", &format!("&{}", pdr_report.get_rust_struct_name()));
+                compare_with_row.line(&table.primary_key_columns.cols.iter()
+                    .map(|name| format!("self.{0} == row.{0}", name.to_lowercase()))
+                    .collect::<Vec<String>>()
+                    .join("\n&& "),
+                );    
+                pk_impl.push_fn(compare_with_row);
+
+                pk_impl.fmt(&mut fmtr)?;
             } else {
                 println!("Cannot find:");
                 dbg!(mms_report);
             }
         }
-        use heck::SnakeCase;
         fs::write(
             format!("src/data_model/{}.rs", data_set.to_snake_case()),
             fmt_str,
