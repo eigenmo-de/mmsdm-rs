cb22150b23a8d6c1798cf67da646976fe858d588
Eric Sheppard <eric@sheppard.cloud>
2021-11-06 12:11:09 +0000

    python codegen - pyarrow schemas

--- codegen/src/python.rs
+++ codegen/src/python.rs
@@ -4,6 +4,47 @@ use serde::{Deserialize, Serialize};
 use std::{collections, fs, iter, str, string};
 
 use crate::{mms, pdr};
+impl mms::DataType {
+    fn as_pyarrow_type(&self) -> String {
+        match self {
+            mms::DataType::Varchar { .. } => "pyarrow.large_utf8()".to_string(),
+            mms::DataType::Char => "pyarrow.large_utf8()".to_string(),
+            mms::DataType::Date => "pyarrow.date32()".to_string(),
+            mms::DataType::DateTime => "pyarrow.date64()".to_string(),
+            mms::DataType::Decimal { precision, scale } => format!(
+                "pyarrow.decimal128({},{})",
+                precision, scale
+            ),
+            mms::DataType::Integer { .. } => "pyarrow.int64()".to_string(),
+        }
+    }
+}
+
+
+impl mms::TableColumn {
+    fn as_pyarrow_field(&self) -> String {
+        format!(
+            "pyarrow.field(\"{name}\", {ty}, {nullable})",
+            name = self.field_name(),
+            ty = self.data_type.as_pyarrow_type(),
+            nullable = if self.mandatory { "False" } else { "True" },
+        )
+    }
+}
+
+impl mms::TableColumns {
+    fn as_pyarrow_schema(&self) -> String {
+        format!("pyarrow.schema([
+        {fields}
+    ])",
+            fields = self
+                .columns
+                .iter()
+                .map(|col| col.as_pyarrow_field())
+                .collect::<Vec<_>>()
+                .join(",\n        "),
+        )    }
+}
 
 pub fn run() -> anyhow::Result<()> {
     let rdr = fs::File::open("mmsdm.json")?;
@@ -43,11 +84,11 @@ def get_row_partition_name(row: List[str]) -> str:
         raise Exception("Row should be a data row but was instead `{}`".format(row[0]))
     mapping = {
 "#);
-    for (data_set, tables) in local_info.into_iter() {
-        for (table_key, table) in tables.into_iter() {
+    for (data_set, tables) in local_info.iter() {
+        for (table_key, table) in tables.iter() {
             let mms_report = mms::Report {
                 name: data_set.clone(),
-                sub_type: table_key,
+                sub_type: table_key.clone(),
             };
 
             if mms_report.should_skip() {
@@ -81,6 +122,63 @@ def get_row_partition_name(row: List[str]) -> str:
         "python/mmsdm/partition.py",
         fmt_str,
     )?;
+
+    let mut fmt_str = String::from(r#"from typing import Optional
+import pyarrow
+"#);
+    let mut extract_str = String::from("def get_schema(data_set: str, sub_type: Optional[str]):
+    mapping = {
+");
+    for (data_set, tables) in local_info.iter() {
+        for (table_key, table) in tables.iter() {
+            let mms_report = mms::Report {
+                name: data_set.clone(),
+                sub_type: table_key.clone(),
+            };
+
+            if mms_report.should_skip() {
+                continue;
+            }
+
+            if let Some(pdr_report) = map.get(&mms_report) {
+                let data_set = pdr_report.name.to_lowercase();
+                let sub_type = pdr_report.sub_type.clone().unwrap_or("null".to_string()).to_lowercase();
+                let schema_fn_name = format!("{}_{}_v{}_schema", data_set, sub_type, pdr_report.version);
+                fmt_str.push_str(&format!(r#"
+def {fn_name}():
+    return {schema}
+"#, 
+                    fn_name = schema_fn_name,
+                    schema = table.columns.as_pyarrow_schema(),
+                ));
+                extract_str.push_str(&format!("        (\"{}\", \"{}\"): {},\n", data_set, sub_type, schema_fn_name));
+
+
+            }
+
+        }
+    }
+
+    extract_str.push_str(r#"    }
+    if sub_type is not None:
+        return mapping[(data_set.lower(), sub_type.lower())]
+    else: 
+        return mapping[(data_set.lower(), "null")]
+    "#);
+
+    fmt_str.push_str("\n");
+    fmt_str.push_str(&extract_str);
+
+    fs::write(
+        "python/mmsdm/data_model.py",
+        fmt_str,
+    )?;
+
+
+    
+
+
+
 
     Ok(())
 }
